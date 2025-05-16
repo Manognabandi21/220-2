e281015e4330b9edf625286f521b34b670d87d55
srithars <sss.srithar@gmail.com>
2025-05-16 12:15:11 +0530

    first commit

---
+++ src/main/java/com/klu/Service.java
@@ -0,0 +1,21 @@
+package com.klu;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@org.springframework.stereotype.Service
+public class Service {
+	@Autowired
+	Repo r1;
+	public String insertData(Product p) {
+		r1.save(p);
+		return "Insert success";
+		
+	}
+	
+	public List<Product> displayData(){
+		return r1.findAll();
+	}
+	
+}
